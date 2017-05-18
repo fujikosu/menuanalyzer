366816a63d4834162c9237e1048cef1757cb3aa4
Parag Paithankar <parag.paithankar@gmail.com>
2017-05-17 19:36:22 -0700

    Added SeeFood

---
+++ SeeFood/SeeFood/SeeFood.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("SeeFood.Droid.MainApplication, SeeFood.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5930cec5cff5fbde0a12056307a3e392a.MainApplication.class, md5930cec5cff5fbde0a12056307a3e392a.MainApplication.__md_methods);
+		
+	}
+}
