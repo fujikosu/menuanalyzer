fd393755c2d8f154b8a8f20d14ee980bf29c24c2
Parag Paithankar <parag.paithankar@gmail.com>
2017-05-18 14:01:27 -0700

    Cleaned

--- SeeFood/SeeFood/SeeFood/MainPage.xaml.cs
+++ SeeFood/SeeFood/SeeFood/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -47,58 +48,7 @@ namespace SeeFood
 
         private async void TakePhoto()
         {
-            await CrossMedia.Current.Initialize();
-
-            if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
-            {
-                //string url = "http://vizmenu.azurewebsites.net/api/getlistsmultipart";
-                string url = "http://vizmenu.azurewebsites.net/api/getlists";
-
-                var mediaOptions = new Plugin.Media.Abstractions.StoreCameraMediaOptions
-                {
-                    Directory = "MenuAnalyzer",
-                    Name = $"{DateTime.UtcNow}.jpg"
-                };
-
-                var file = await CrossMedia.Current.TakePhotoAsync(mediaOptions);
-
-                /*HttpClient client = new HttpClient();
-
-                
-                MultipartFormDataContent content = new MultipartFormDataContent();
-                ByteArrayContent baContent = new ByteArrayContent(StreamToByteArray(file.GetStream()));
-
-                content.Add(baContent, "File", "filename.ext");
-                
-
-                //upload MultipartFormDataContent content async and store response in response var
-                var response =
-                  await client.PostAsync(url, content);
-
-                //read response result as a string async into json var
-                var responsestr = response.Content.ReadAsStringAsync().Result;
-                //Socket s = new Socket(SocketType.Stream);
-
-
-                //byte[] filedata = GetByteArray(file.GetStream());*/
-                var request = (HttpWebRequest)WebRequest.Create("http://vizmenu.azurewebsites.net/api/getlists");
-
-                request.Method = "POST";
-                request.ContentType = "multipart/form-data";
-
-                var data = StreamToByteArray(file.GetStream());
-
-                //request..ContentLength = data.Length;
-
-                using (var stream = await request.GetRequestStreamAsync())
-                {
-                    stream.Write(data, 0, data.Length);
-                }
-
-                var response = await request.GetResponseAsync();
-
-                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-            }
+            
         }
     }
 }
