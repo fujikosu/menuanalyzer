843a4888921ed24535606da0f766d05a3c187ba1
Parag Paithankar <parag.paithankar@gmail.com>
2017-05-17 19:47:07 -0700

    Added Photo Taking Code

--- SeeFood/SeeFood/SeeFood.Android/obj/Debug/SeeFood.Droid.MainPage.xaml.g.cs
+++ SeeFood/SeeFood/SeeFood.Android/obj/Debug/SeeFood.Droid.MainPage.xaml.g.cs
@@ -9,16 +9,15 @@
 //------------------------------------------------------------------------------
 
 namespace SeeFood {
-    using System;
-    using Xamarin.Forms;
-    using Xamarin.Forms.Xaml;
     
     
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\papaitha\\Documents\\GitHub\\menuanalyzer\\SeeFood\\SeeFood\\SeeFood\\MainPage." +
+        "xaml")]
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
-            this.LoadFromXaml(typeof(MainPage));
+            global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
         }
     }
 }
--- SeeFood/SeeFood/SeeFood/MainPage.xaml.cs
+++ SeeFood/SeeFood/SeeFood/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,28 @@ namespace SeeFood
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            TakePhoto();
+        }
+
+        private async void TakePhoto()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
+            {
+                var mediaOptions = new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "MenuAnalyzer",
+                    Name = $"{DateTime.UtcNow}.jpg"
+                };
+
+                var file = await CrossMedia.Current.TakePhotoAsync(mediaOptions);
+
+                //byte[] filedata = GetByteArray(file.GetStream());
+            }
+        }
+    }
 }
